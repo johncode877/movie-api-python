67f0ba792aac9bf05d1ca967a0fec8139db53320
john code <john.code.877@gmail.com>
2024-09-01 21:18:12 -0500

    validacion de datos del esquema 2

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ class Movie(BaseModel):
     title: str = Field(min_length=5,max_length=15)
     overview: str = Field(min_length=15,max_length=50)
     year: int = Field(le=2022)
-    rating: float
-    category: str
-
+    rating: float = Field(ge=1,le=10)
+    category: str = Field(min_length=5,max_length=15)
+     
     class Config:
         json_schema_extra = {
            "examples" : [
