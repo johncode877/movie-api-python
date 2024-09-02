42044add659e325a9d7755b06f8770df10ad0e2d
john code <john.code.877@gmail.com>
2024-09-01 22:28:17 -0500

    trabajando con JSONResponse y modelo de respuesta

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI,Body,Path,Query
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse,JSONResponse
 from pydantic import BaseModel,Field
-from typing import Optional
+from typing import List, Optional
 
 
 # instancia de fastapi
@@ -59,36 +59,39 @@ movies = [
 def message():
     return HTMLResponse('<h1>Hello World</h1>')
 
-@app.get('/movies',tags=['movies'])
-def get_movies():
-    return movies
+@app.get('/movies',tags=['movies'],response_model=List[Movie])
+def get_movies() -> List[Movie]:
+    return JSONResponse(content=movies)
 
 # id es un parameter path
-@app.get('/movies/{id}',tags=['movies'])
-def get_movie(id:int=Path(ge=1,le=2000)):
+@app.get('/movies/{id}',tags=['movies'],response_model=Movie)
+def get_movie(id:int=Path(ge=1,le=2000)) -> Movie:
     for item in movies:
         if item["id"] == id:
-            return item
+            return JSONResponse(content=item)
     
-    return []
+    return JSONResponse(content=[])
 
 # category es un parameter query
-@app.get('/movies/',tags=['movies'])
-def get_movies_by_category(category:str=Query(min_length=5,max_length=15)):
-   return [item for item in movies if item["category"] == category]
+@app.get('/movies/',tags=['movies'],response_model=List[Movie])
+def get_movies_by_category(category:str=Query(min_length=5,max_length=15)) -> List[Movie]:
+   data = [item for item in movies if item["category"] == category]
+   return JSONResponse(content=data)
 
 # para que los campos se han considerados
 # como parte del request body y no como variable query 
 # es necesario asignarles que son de tipo Body	       
-@app.post('/movies',tags=['movies'])
-def create_movie(movie:Movie):
-    movies.append(movie)
+@app.post('/movies',tags=['movies'], response_model=dict)
+def create_movie(movie:Movie) -> dict:
+    # convierte el objeto Movie (pydantic) a un diccionario
+    # para que pueda ser añadido a la lista de diccionarios
+    # de movies 
+    movies.append(movie.model_dump())
+    return JSONResponse(content={"message":"Se ha registrado la pelicula"})
 
-    return movies
 
-
-@app.put('/movies/{id}',tags=['movies'])
-def update_movie(id: int, movie: Movie):
+@app.put('/movies/{id}',tags=['movies'],response_model=dict)
+def update_movie(id: int, movie: Movie) -> dict:
     for item in movies:
         if item["id"] == id:
             item["title"] = movie.title   
@@ -98,14 +101,15 @@ def update_movie(id: int, movie: Movie):
             item["year"] = movie.year   
             item["category"] = movie.category
 
-    return movies
+    return JSONResponse(content={"message":"Se ha modificado la pelicula"})
 
-@app.delete('/movies/{id}',tags=['movies'])
-def delete_movie(id: int): 
+# response_model indica el modelo de respuesta
+@app.delete('/movies/{id}',tags=['movies'],response_model=dict)
+def delete_movie(id: int) -> dict: 
     for item in movies:
     	if item["id"] == id:
             movies.remove(item)
-            return movies
+    return JSONResponse(content={"message":"Se ha eliminado la pelicula"})
 
 
 
