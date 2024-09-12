1143106ed8eee34371cb2a8cafc3d4d0ff38d1b4
johncode877 <118542955+johncode877@users.noreply.github.com>
2024-09-12 13:13:31 -0500

    Update hola_lenguaje.py

--- hola_lenguaje.py
+++ hola_lenguaje.py
@@ -2,8 +2,9 @@
 import os
 
 def hola_lenguaje():
-  nombre = os.getenv("USERNAME")	
-  print(f"!Hola , {nombre} desde GitHub!")
+  nombre = os.getenv("USERNAME")
+  language = os.getenv("LANGUAGE")  
+  print(f"!Hola , {nombre} mi lenguaje favorito es {language} !")
 
 if __name__ == '__main__':
   hola_lenguaje()
